e5c4ab6a103fd606df0909975fb8d6ba8315f9bb
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2019-12-08 19:37:00 -0300

    text mining

--- clusters.R
+++ clusters.R
@@ -5,16 +5,17 @@ set.seed(1)
 #gera um formato não tidy para avaliar a evolução anual como parâmetro do cluster
 itr_spread<-
 itr_municipio %>% 
+  filter(ano>2000) %>%
   spread(ano, valor_arrecadado)
 
 #busca identificar qual o número ideal de grupos
 sil_width <- map_dbl(2:10,  function(k){
-  model<- pam(x = itr_spread[,4:22], k = k)
+  model<- pam(x = itr_spread[,4:21], k = k)
   model$silinfo$avg.width
 })
 
 #O número ideal de grupos é 2
-model<- pam(x = itr_spread[,4:22], k = 2)
+model<- pam(x = itr_spread[,4:21], k = 2)
 
 #cria uma tabela 
 itr_cluster<-
@@ -23,16 +24,21 @@ itr_spread%>%
 
 
 itr_cluster %>%
-  gather(key = "ano", value = "valor_arrecadado", -c(1:3,23)) %>%
+  gather(key = "ano", value = "valor_arrecadado", -c(1:3,22)) %>%
   ggplot(aes(x= ano, y= valor_arrecadado )) +
-  geom_line(aes(group = municipio,  color= factor(cluster)))
+  geom_line(aes(group = municipio,  color= factor(cluster))) +
+  scale_y_log10()
 
 #Informa o número de elementos por agrupamento formado
-itr_cluster %>%
-  group_by(cluster) %>%
+
+itr_cluster %>%  
+group_by(cluster) %>%
   summarise(
-    n()
-  )
+    quantidade= n()
+  ) %>%
+ggplot()  +
+  geom_col(aes(x=factor(cluster), y= quantidade, fill= factor(cluster)))  +
+  coord_flip()
   
 
 
